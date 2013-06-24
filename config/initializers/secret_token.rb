12eadb38f9445972868dc42251b263351d138609
caok <caok1231@gmail.com>
2013-06-24 20:56:22 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Union::Application.config.secret_token = '3339de1a9ffef964e509887a28122898be9d2d47e722a73af711478c0e09075ab2f6d07f0b92a582d356c45f532e4804d50901c00cfadf01f837369936307439'
