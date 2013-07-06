3a9a7069ff861bc6e1109016489baa8ccc93062e
vincent178 <vincent.007.cn@gmail.com>
2013-07-06 17:07:55 +0800

    ongoing

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,7 +3,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.json
   def index
-    @projects = Project.where("status = 'ongoing'")
+    @projects = Project.where(status: Project::ONGOING)
 
     respond_to do |format|
       format.html # index.html.erb
