38e19147b7dbd6db4fbc36259471cc1ee1823455
caok <caok1231@gmail.com>
2013-07-07 16:50:10 +0800

    fix some bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ActiveRecord::Base
   belongs_to :grade
 
   validates :name, uniqueness: true
-  
+
   before_save :default_user_grade
 
   scope :without_user, lambda{|user| user ? {:conditions => ["id != ?", user.id]} : {} }
@@ -126,6 +126,4 @@ class User < ActiveRecord::Base
     grade = Grade.find_by_weights 1
     self.grade = grade
   end
-
-
 end
