5ae4e51855a4defb506c1330df9a1b769e55cf98
mouseshi <linhongshimouse@gmail.com>
2013-07-07 09:04:40 +0800

    rm comment out codes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    #@users = User.without_user(current_user).page(params[:page]).per(30)
     @users = User.page params[:page]
   end
 
