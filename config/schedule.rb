186faf1d36148b6b9b020d16e7f3805023cf87ff
Ken Lu <ken@seeice.com>
2013-07-08 08:01:01 -0400

    updated not only score but also fetch all bay6 commit

--- config/schedule.rb
+++ config/schedule.rb
@@ -23,5 +23,6 @@ set :rbenv_version, ENV['RBENV_VERSION'] || "jruby-1.6.7.2"
 set :job_template, "env RBENV_VERSION=#{rbenv_version} bash -l -c ':job'"
 
 every :hour do
+  runner "Commit.fetch_bay6"
   runner "User.update_all_scores"
 end
