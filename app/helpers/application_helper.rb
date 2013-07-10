9348095b441dcda3fa90b030135d629e623df496
kaku87 <ghjcumt2008@gmail.com>
2013-07-11 02:50:01 +0900

    add sort for grade

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -49,4 +49,11 @@ module ApplicationHelper
   def format_year(time)
     (time.year == Time.zone.now.year) ? :stamp : :stamp_with_year
   end
+
+  def sortable(column, title = nil)
+      title ||= column.titleize
+      css_class = column == sort_column ? "current #{sort_direction}" : nil
+      direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+      link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+  end
 end
