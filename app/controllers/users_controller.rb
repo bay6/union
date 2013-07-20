2c71077229aecc1c26319ecfb08727c307c1388b
William Herry <WilliamHerryChina@Gmail.com>
2013-07-21 03:13:59 +0800

    make all column of user ranking sortable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 #encoding: utf-8
 class UsersController < InheritedResources::Base
   before_filter :authenticate_user!
+  helper_method :sort_column,:sort_direction
 
   def index
     @users = User.page params[:page]
@@ -25,7 +26,23 @@ class UsersController < InheritedResources::Base
     redirect_to :users, notice: "User was successfully updated " if @user.update_attributes! params[:user]
   end
 
+  def ranking
+    @users = User.cuscom_sort(sort_column, sort_direction)
+    @users = Kaminari.paginate_array(@users).page params[:page]
+  end
+
   private
+
+  def sort_column
+    extra_columns = %w(total month week first_commit_date grades.weights)
+    columns = User.column_names + extra_columns
+    columns.include?(params[:sort]) ? params[:sort] : "total"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
+  end
+
 
   def last_7_days
     min = Date.today - 8
