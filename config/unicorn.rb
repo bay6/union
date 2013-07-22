ad1798352251d5990f1a9ccc061dee298a5a5b1d
mouseshi <linhongshimouse@gmail.com>
2013-07-23 01:30:28 +0800

    listen union socket

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,10 +1,12 @@
 # config/unicorn.rb
+RAILS_ROOT = File.expand_path("../..", __FILE__)
+
 worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
 timeout 240
 preload_app true
 
 listen 8081, :tcp_nopush => false
-listen "#{RAILS_ROOT}/tmp/socket/union.socket"
+listen "#{RAILS_ROOT}/tmp/sockets/union.socket"
 pid "#{RAILS_ROOT}/tmp/pids/unicorn.pid"
 stderr_path "#{RAILS_ROOT}/log/unicorn.err.log"
 stdout_path "#{RAILS_ROOT}/log/unicorn.out.log"
