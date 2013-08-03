205573d28b0257c706b4d1a922324bdf91e684e3
Ken Lu <ken@seeice.com>
2013-08-03 19:43:06 -0400

    move the project fetch bay6 function from seed to model #236

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,22 +19,8 @@ Grade.find_or_create_by_name("初级", :weights => 2)
 Grade.find_or_create_by_name("中级", :weights => 4)
 Grade.find_or_create_by_name("高级", :weights => 8)
 
-# project init
-@client = Octokit::Client.new(:login => "ken0", :password => "password9")
-brepos = @client.repos( 'bay6')
-brepos.each do |repo|
-  p = Project.find_or_create_by_name(repo.name,
-                                 :website => repo.html_url,
-                                 :description => repo.description,
-                                 :grade_id => 1,
-                                 :user_id => 1,
-                                 :status => Project::ONGOING
-                                )
-  p.save! :validate => false
-end
-
+Project.get_bay6_repos
 Repository.get_bay6_repos
-
 Project.all.each{|p| Repository.find_by_name(p.name).update_column('project_id', p.id)}
 
 Participation.all.each{|p|p.update_column('created_at', 30.days.ago)}
