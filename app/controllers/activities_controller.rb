d39046d8010139a8adf6ee03bfe7c2cabb28038a
caok <caok1231@gmail.com>
2013-08-04 09:08:01 +0800

    modify the index of activities

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -5,7 +5,7 @@ class ActivitiesController < InheritedResources::Base
   load_resource
 
   def index
-    @activities = Activity.where('status != 3')
+    @activities = Activity.where('status != 3').page params[:page]
 
     respond_to do |format|
       format.html # index.html.erb
