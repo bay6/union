a9c1ade8b0ca25a994467d5d868b3007fd4063a7
caok <caok1231@gmail.com>
2013-08-07 21:53:49 +0800

    fix the bug of ranking

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,10 @@
+#encoding:utf-8
 module ApplicationHelper
   def format_date(datetime)
     if datetime.nil?
       datetime
+    elsif datetime == "0"
+      "不存在"
     else
       datetime.strftime("%Y-%m-%d")
     end
